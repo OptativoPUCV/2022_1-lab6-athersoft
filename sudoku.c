3b07205beac5f11d3374d4ef0b259f391a662796
athersoft <>
2022-05-12 15:46:29 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -44,7 +44,16 @@ void print_node(Node* n){
 }
 
 int is_valid(Node* n){
-
+    for(int i = 0; i < 9; i++){
+      for(int j = 0, j < 9; j++){
+        int num = n->sudo[i][j];
+        for(int k = 0; k<9 k++){
+          if(num == sudo[k][j] || num == sudo[j][k]){
+            return 0;
+          }
+        }
+      }
+    }
     return 1;
 }
 

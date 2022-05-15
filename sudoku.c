1808e83e3b6089d42dff0c7b3e8dd4a7e2621798
athersoft <>
2022-05-15 18:23:50 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -77,11 +77,11 @@ int is_valid(Node* n){
       for(int k = i; k < i+3; k++){
         for(int u = j; u < j+3; j++){
           if(n->sudo[k][u] != 0){
-            if(numeros[n -> sudo[k][u]] == 0){
-              numeros[n->sudo[k][u]] += 1;
-            }else{
-              return 0;
-            }
+            //if(numeros[n -> sudo[k][u]] == 0){
+              numeros[n->sudo[k][u]] = 1;
+            //}else{
+              //return 0;
+            //}
           }
         }
       }
